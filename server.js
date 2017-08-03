a05be829c70ff3e30892a4d9f995044dc1a6c160
Jake Thornberry <wjthornberry@gmail.com>
2017-08-02 21:02:04 -0500

    Add roster, welcome, student roster

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
+// Comment
 var express = require('express');
 var app = express();
 
 var PORT = 3000;
 
+var roster = [
+    {
+        name: 'Mark'
+    },
+    {
+        name: 'Allen'
+    },
+    {
+        name: 'Techson the Great'
+    }
+]
+
+// define a get route that returns a welcome message
+app.get('/', function (req, res) {
+    res.send('Welcome.');
+});
+app.get('/students', function (req, res) {
+    res.json(roster);
+});
+// return a student by ID (index in the array)
+// URL or route with a paramter that we pass in
+// like calling a function and passing in a variable
+// Can do the same thing with the route
+// General route path, but here's a special param
+app.get('/students/:studentId', function (req, res) {
+    res.json(roster[
+        // params object is from Express       
+        req.params.studentId]);
+});
+
+// server listening
 app.listen(PORT, function() {
     console.log(`Listening on PORT ${PORT}`);
 });
